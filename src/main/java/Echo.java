e7b4f1c668eee3829e925cc0e3e213425691433a
chiyang <chiyangg@gmail.com>
2025-09-14 10:32:46 +0800

    add: Deadline subclass, Event subclass, refactor: extract horizontalLine()

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Echo {
     public static void main(String[] args) {
-        System.out.println("___________________________________________");
+        horizontalLine();
         System.out.println("Hello! I'm Echo");
         System.out.println("What can I do for you?");
-        System.out.println("___________________________________________");
+        horizontalLine();
 
         Scanner sc = new Scanner(System.in);
         String userInput;
@@ -16,59 +16,87 @@ public class Echo {
             userInput = sc.nextLine();
 
             if (userInput.equalsIgnoreCase("bye")) {
-                System.out.println("___________________________________________");
+                horizontalLine();
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("___________________________________________");
+                horizontalLine();
                 break;
             }
             else if (userInput.equalsIgnoreCase("list")) {
-                System.out.println("___________________________________________");
+                horizontalLine();
                 for (int i = 0; i < taskCounter; i++) {
                     System.out.println(i+1 + ". " + tasks[i]);
                 }
-                System.out.println("___________________________________________");
+                horizontalLine();
             }
 
             else if (userInput.startsWith("mark ")) {
                 int index = Integer.parseInt(userInput.split(" ")[1]) - 1;
                 tasks[index].markAsDone();
-                System.out.println("___________________________________________");
+                horizontalLine();
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println("  " + tasks[index]);
-                System.out.println("___________________________________________");
+                horizontalLine();
             }
 
             else if (userInput.startsWith("unmark ")) {
                 int index = Integer.parseInt(userInput.split(" ")[1]) - 1;
                 tasks[index].markAsNotDone();
-                System.out.println("___________________________________________");
+                horizontalLine();
                 System.out.println("Nice! I've marked this task as not done yet:");
                 System.out.println("  " + tasks[index]);
-                System.out.println("___________________________________________");
+                horizontalLine();
             }
 
             else if (userInput.startsWith("todo ")) {
                 String description = userInput.substring(5).trim();
                 tasks[taskCounter] = new Todo(description);
                 taskCounter++;
-                System.out.println("___________________________________________");
-                System.out.println("Got it. I've added this task");
-                System.out.println("  " + tasks[taskCounter-1]);
-                System.out.println("Now you have " + taskCounter + " tasks in the list");
-                System.out.println("___________________________________________");
+                horizontalLine();
+                System.out.println(tasks[taskCounter-1].addedMessage(taskCounter));
+                horizontalLine();
+            }
+
+            else if (userInput.startsWith("deadline ")) {
+                String withoutCommand = userInput.substring(9);
+                String[] parts = withoutCommand.split("/by", 2);
+                String description = parts[0].trim();
+                String by = parts[1].trim();
+                tasks[taskCounter] = new Deadline(description,by);
+                taskCounter++;
+                horizontalLine();
+                System.out.println(tasks[taskCounter-1].addedMessage(taskCounter));
+                horizontalLine();
+            }
+            else if (userInput.startsWith("event ")) {
+
+                String withoutCommand = userInput.substring(6);
+                String[] parts = withoutCommand.split("/from", 2);
+                String description = parts[0].trim();
+                String[] timeParts = parts[1].split("/to", 2);
+                String from = timeParts[0].trim();
+                String to = timeParts[1].trim();
+                tasks[taskCounter] = new Event(description, from, to);
+                taskCounter++;
+                horizontalLine();
+                System.out.println(tasks[taskCounter-1].addedMessage(taskCounter));
+                horizontalLine();
             }
 
             else {
                 tasks[taskCounter] = new Task(userInput);
                 taskCounter++;
-                System.out.println("___________________________________________");
+                horizontalLine();
                 System.out.println("added: " + userInput);
-                System.out.println("___________________________________________");
+                horizontalLine();
             }
         }
 
         sc.close();
 
 
+    }
+
+    public static void horizontalLine() {
+        System.out.println("___________________________________________");
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,6 +2,7 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+
     public Task(String description) {
         this.description = description;
         this.isDone = false;
@@ -17,6 +18,10 @@ public class Task {
 
     public void markAsNotDone() {
         isDone = false;
+    }
+
+    public String addedMessage(int taskCounter) {
+        return "Got it. I've added this task: \n" + "  " +this + "\nNow you have " + taskCounter + " tasks in the list ";
     }
 
     @Override
